55027dac4bcd01f1944c57bac3f79007fc9ded15
lrpuppi <lrpuppi@gmail.com>
2019-03-16 16:08:14 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/lrp/workshopmongo/services/UserService.java
+++ src/main/java/com/lrp/workshopmongo/services/UserService.java
@@ -37,6 +37,17 @@ public class UserService {
 		repository.delete(id);
 	}
 	
+	public User update(User obj) {
+		User newObj = repository.findOne(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+	
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
